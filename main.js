8a8c0ab0ff71f13b8efa713bd7dd2a90cfe72794
Andres Badillo <r.andres.badillo@gmail.com>
2023-03-09 13:07:25 -0500

    css e importaciones

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import { setupCounter } from './counter.js';
+
+import buttonStyles from './button.module.css';
+import imageStyles from './image.module.css';
+import image from './PhotoRoom.png';
+
+import { user } from './data.json';
+
+const modules = import.meta.glob( './modules/*.js' );
+console.log(modules);
+for (const path in modules) {
+  modules[path]().then((module) => {
+    module.load();
+  });
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -10,14 +24,22 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Andrés!</h1>
+    <img id="img" />
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
+    <pre>${JSON.stringify(user)}</pre>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
 `
+
+document.getElementById('counter').className = buttonStyles.btn;
+
+const imagen = document.getElementById('img');
+imagen.src = image;
+imagen.className = imageStyles.img;
 
 setupCounter(document.querySelector('#counter'))
