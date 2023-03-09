ceed391f026a614c40b76167c19d334e9297bc81
Andres Badillo <r.andres.badillo@gmail.com>
2023-03-09 14:30:35 -0500

    Last commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { resolve } from 'path';
 
-export default defineConfig (() => {
+export default defineConfig (({ command, mode }) => {
     const port = 8080;
 
-    return {
-        server: {
-            port: port
+    const env = loadEnv(mode, process.cwd());
+    console.log(env.VITE_NAME);
+
+    console.log(command, mode);
+
+    if (mode === 'development') {
+        console.log('modo desarrollo');
+        return {
+            server: {
+                port: port
+            }
+        }
+    } else {
+        console.log('modo produccion');
+        return {
+            build: {
+                lib: {
+                    entry: resolve(__dirname, 'lib', 'main.js'),
+                    name: 'demo',
+                    fileName: (format) => `demo.${format}.js`,
+                }
+            }
         }
     }
+    
 });
